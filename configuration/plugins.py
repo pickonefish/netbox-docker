1f825be857e16d82c27f64f9898153b3a9ff78f3
pickonefish <pickonefish@gmail.com>
2024-05-27 08:01:41 +0800

    netbox acl

--- configuration/plugins.py
+++ configuration/plugins.py
@@ -4,7 +4,18 @@
 # To learn how to build images with your required plugins
 # See https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
 
-PLUGINS = ["netbox_secrets", "netbox_topology_views"]
+PLUGINS = ["netbox_secrets", "netbox_topology_views", "netbox_acls"]
+
+PLUGINS_CONFIG = {
+    'netbox_topology_views': {
+        'static_image_directory': 'netbox_topology_views/img',
+        'allow_coordinates_saving': True,
+        'always_save_coordinates': True
+    },
+    "netbox_acls": {
+        "top_level_menu": True # If set to True the plugin will add a top level menu item for the plugin. If set to False the plugin will add a menu item under the Plugins menu item.  Default is set to True.
+    },
+}
 
 # PLUGINS = ["netbox_bgp"]
 
